a7722ef5dedba3b00a77c23c2c74c80b03d4b822
sarthaksavvy <sarthak@bitfumes.com>
2019-07-08 16:28:47 +0530

    3. submit login form

--- src/Login.js
+++ src/Login.js
@@ -1,21 +1,34 @@
 import React, { Component } from "react";
 
 export default class Login extends Component {
+    constructor(props){
+        super(props)
+        this.state = {email:'',password:'',errors:{}}
+    }
+    handleForm = (e) => {
+        e.preventDefault();
+    }
+    handleInput= (e) => {
+        e.preventDefault();
+        const name = e.target.name
+        const value = e.target.value
+        this.setState({[name]:value})
+    }
   render() {
     return (
       <div className="flex">
         <div className="w-1/3" />
         <div className="w-1/3 mt-10 p-4 bg-white">
-          <form className="border border-gray-500">
+          <form className="border border-gray-500" onSubmit={this.handleForm}>
             <div className="p-4">
               <h1 className="text-lg border-b border-gray-500">Ping Here</h1>
               <div className="mt-4">
                 <label>Email</label>
-                <input type="email" name="email" placeholder="Lovely Email" className="mt-1 p-2 bg-gray-200 rounded border border-gray-400 w-full" />
+                <input type="email" name="email" placeholder="Lovely Email" onChange={this.handleInput} className="mt-1 p-2 bg-gray-200 rounded border border-gray-400 w-full" />
               </div>
               <div className="mt-4">
                 <label>Password</label>
-                <input type="password" name="password" placeholder="Super Duper Secret Password" className="mt-1 p-2 bg-gray-200 rounded border border-gray-400 w-full" />
+                <input type="password" name="password" onChange={this.handleInput} placeholder="Super Duper Secret Password" className="mt-1 p-2 bg-gray-200 rounded border border-gray-400 w-full" />
               </div>
               <div className="mt-4">
                 <input type="submit" className="mt-1 p-2 border border-gray-400 rounded cursor-pointer bg-purple-600 text-white" />
