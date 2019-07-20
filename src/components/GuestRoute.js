347c4df93ff0d30cffd8462a24603a1f27f310ea
sarthaksavvy <sarthak@bitfumes.com>
2019-07-20 19:49:51 +0530

    11 feth user before render

--- src/components/GuestRoute.js
+++ src/components/GuestRoute.js
@@ -1,14 +1,13 @@
 import { Route, Redirect } from "react-router-dom";
 import React from "react";
-import cookie from "js-cookie";
+import { connect } from "react-redux";
 
 const GuestRoute = ({ component: Component, ...rest }) => {
-  const token = cookie.get("token");
   return (
     <Route
       {...rest}
       render={props =>
-        !token ? (
+        !rest.loggedIn ? (
           <Component {...props} />
         ) : (
           <Redirect
@@ -23,4 +22,9 @@ const GuestRoute = ({ component: Component, ...rest }) => {
   );
 };
 
-export default GuestRoute;
+const mapStateToProps = state => {
+  return {
+    loggedIn: state.auth.loggedIn
+  };
+};
+export default connect(mapStateToProps)(GuestRoute);
--- src/index.js
+++ src/index.js
@@ -5,13 +5,27 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import store from "./store/index";
 import { Provider } from "react-redux";
+import axios from "axios";
+import cookie from "js-cookie";
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
-);
+const token = cookie.get("token");
+const render = () => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById("root")
+  );
+};
+if (token) {
+  axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+  axios.post("http://localhost:8000/api/auth/me").then(res => {
+    store.dispatch({ type: "SET_LOGIN", payload: res.data });
+    render();
+  });
+} else {
+  render();
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
